d6e6d280b2186962be3e0e5b99088c1829c6a596
Jaydeep Ray <jaydeepray1508@gmail.com>
2026-08-31 18:21:08 +0530

    feat: make program interactive

--- calculator.c
+++ calculator.c
@@ -11,39 +11,70 @@
 // main function
 int main(void)
 {
-    int opt_num, num1, num2, res;
-    printf("\n 1. Addition \n 2. Substraction \n 3. Multiplication \n 4. Division \n 5. Exponentiation");
+    int opt_num, res, opr1, opr2, opr;
+    printf("\n %-40s %-40s %-40s", "1. Addition", "2. Substraction", "3. Multiplication");
+    printf("\n %-40s %-40s %-40s", "4. Division", "5. Exponentiation", "6. Sqaure");
+    printf("\n %-40s %-40s %-40s", "7. Log (base e)", "8. Log (base 10)", "9. Sine (angle in radians)");
+    printf("\n %-40s %-40s", "10. Cosine (angle in radians)", "11. Tangent (angle in radians)");
+    printf("\n Enter `-1` to exit.");
     printf("\n Enter operation number: ");
     scanf("%d", &opt_num);
-    printf("\n Enter the first number: ");
-    scanf("%d", &num1);
-    printf("\n Enter the second number: ");
-    scanf("%d", &num2);
-
-    switch (opt_num)
+    
+    while (opt_num != -1)
     {
-        case 1:
-            res = num1 + num2;
-            printf("\n %d + %d = %d", num1, num2, res);
+        if ((opt_num > 0) && (opt_num <= 5))
+        {
+            printf("\n Enter the first operand: ");
+            scanf("%d", &opr1);
+            printf("\n Enter the second operand: ");
+            scanf("%d", &opr2);
+        }
+        else if ((opt_num > 5) && (opt_num <= 11))
+
+        {
+            printf("\n Enter the oprand: ");
+            scanf("%d", &opr);
+        }
+        else
+        printf("\n Wrong operation number provided.");
+        
+        switch (opt_num)
+        {
+            case 1:
+            res = opr1 + opr2;
+            printf("\n %d + %d = %d", opr1, opr2, res);
             break;
-        case 2:
-            res = num1 - num2;
-            printf("\n %d - %d = %d", num1, num2, res);
+            case 2:
+            res = opr1 - opr2;
+            printf("\n %d - %d = %d", opr1, opr2, res);
             break;
-        case 3:
-            res = num1 * num2;
-            printf("\n %d * %d = %d", num1, num2, res);
+            case 3:
+            res = opr1 * opr2;
+            printf("\n %d * %d = %d", opr1, opr2, res);
             break;
-        case 4:
-            res = num1 / num2;
-            printf("\n %d / %d = %d", num1, num2, res);
+            case 4:
+            res = opr1 / opr2;
+            printf("\n %d / %d = %d", opr1, opr2, res);
             break;
-        case 5:
-            res = pow(num1, num2);
-            printf("\n %d raised to the power %d is: %d", num1, num2, res);
+            case 5:
+            res = pow(opr1, opr2);
+            printf("\n %d raised to the power %d is: %d", opr1, opr2, res);
             break;
-        default:
+            default:
             printf("\n Wrong operation number is entered.");
+        }
+        printf("\n Press enter to continue...");
+        getchar();
+        getchar();
+        printf("\033[2J\033[3J\033[H");
+        fflush(stdout);
+        printf("\n %-40s %-40s %-40s", "1. Addition", "2. Substraction", "3. Multiplication");
+        printf("\n %-40s %-40s %-40s", "4. Division", "5. Exponentiation", "6. Sqaure");
+        printf("\n %-40s %-40s %-40s", "7. Log (base e)", "8. Log (base 10)", "9. Sine (angle in radians)");
+        printf("\n %-40s %-40s", "10. Cosine (angle in radians)", "11. Tangent (angle in radians)");
+        printf("\n Enter `-1` to exit.");
+        printf("\n Enter operation number: ");
+        scanf("%d", &opt_num);
     }
     return 0;
 }
